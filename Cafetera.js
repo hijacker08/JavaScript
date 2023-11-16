2ed1d0cc61c101545b84c3d5ce6d07fc527df2ac
hijacker08 <christian.medina2000@gmail.com>
2023-11-16 00:31:27 -0400

    hola este es el primer commit

---
+++ Cafetera.js
@@ -0,0 +1,12 @@
+var resultado ="capuchino"
+function cafetera (ingrediente1){
+    if(ingrediente1=="leche"){
+        var resultado = "otra cosa"
+        var resultado = "otra cosa 2"
+        var resultado = "otra cosa 2"
+        return resultado
+    }
+}
+
+var tasa = cafetera("leche")
+console.log(tasa)
---
+++ index.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+char variable[42] ="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";
+using namespace std;
+int main(){
+    cout << "Hola Christian";
+}
+
